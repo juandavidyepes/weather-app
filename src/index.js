315cae97c51e7772dd95a44a213b0858d715f952
juandavidyepes <judavidy@hotmail.com>
2021-03-20 15:57:05 -0500

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <img
-      src='https://www.gradiweb.com/wp-content/uploads/2019/04/logo.png'
-      alt='logo'
-      className='logo'
-    />
+    <div className='logoContainer'>
+      <img
+        src='https://www.gradiweb.com/wp-content/uploads/2019/04/logo.png'
+        alt='logo'
+        className='logo'
+      />
+    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
